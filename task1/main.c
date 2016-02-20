09992bb3a40db3fb2bfc42a691eb47e242b9dd8b
Nemzs <nemzs1337@gmail.com>
2016-02-20 05:04:48 +0300

    final_task1

--- task1/main.c
+++ task1/main.c
@@ -9,26 +9,33 @@ int main ()
     ll **matrix = (ll **)malloc((n+1) * sizeof(ll *));
     int i,j;
     for(i = 0; i < n; i++)
-    matrix[i] = (ll *)malloc((n+1) * sizeof(ll));
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
+        matrix[i] = (ll *)malloc((n+1) * sizeof(ll));
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
             matrix[i][j]=(i+1)*(j+1);
         }
     }
     int x1,x2,y1,y2;
-    while(1){
+    while(1)
+    {
         scanf("%d",&x1);
-        if(x1==0){
+        if(x1==0)
+        {
             break;
         }
         scanf("%d%d%d",&y1,&x2,&y2);
         int format = 0, cp = matrix[x2-1][y2-1];
-        while(cp>0){
+        while(cp>0)
+        {
             format++;
             cp/=10;
         }
-        for(i=x1;i<=x2;i++){
-            for(j=y1;j<=y2;j++){
+        for(i=x1; i<=x2; i++)
+        {
+            for(j=y1; j<=y2; j++)
+            {
                 printf("%*lld ",format,matrix[i-1][j-1]);
             }
             printf("\n");
