b7101128b020b91755aa82af5480520a2bf21a0f
Nemzs <nemzs1337@gmail.com>
2016-05-19 23:14:39 +0300

    fixed endIf

--- task6/lazy_string.h
+++ task6/lazy_string.h
@@ -21,32 +21,92 @@ class lazy_string {
     };
 
 public:
+    /*
+        creates an empty string
+        *return lazy_string (size = length = 0, undefined capacity);
+    */
     lazy_string();
 
+    /*
+        creates an lazy_string containing chars from str
+        *arg std::string
+    */
     lazy_string(const std::string str);
 
+    /*
+        returns lazy_string that contain chars between [st, st + len)
+        *arg st - first char of the substring
+        *arg len - len of the substring
+        *return An empty string if st==size()
+        *return substring
+        *throws std::out_of_range("lazy_string") if len > size()
+    */
     lazy_string substr(size_t st, size_t len);
 
+    /*
+
+        returns lazy_string that contain chars from st to end of the string
+        *arg st - first char of the substring
+        *return substring from st to end of the string
+        *throws std::out_of_range("lazy_string") if len > size()
+    */
     lazy_string substr(size_t st);
 
+    /*
+        returns a reference to the char at st position
+        function check if valid position, if not throws std::out_of_range
+        *arg st - first char of the substring
+        *return a reference to the char
+        *throws std::out_of_range("lazy_string") if(st >= size() || st < 0)
+    */
     char_ref at(size_t index);
 
     char at(size_t index) const;
 
+    /*
+        return the number of char in the string
+        *return the number of chars in the string
+    */
     size_t length();
 
+    /*
+        return the number of char in the string
+        *return the number of chars in the string
+    */
     size_t size();
 
-    friend std::istream &operator>>(std::istream &in, lazy_string &ns);
+    /*
+        Get the string from the input stream in lazy, old version of lazy replaced with new string
+        *arg in std::istream input stream wich chars are extracting
+        *lazy lazy lazy_string string to replace with new excracted string
+        *return in argument
+    */
+    friend std::istream &operator>>(std::istream &in, lazy_string &lazy);
 
-    friend std::ostream &operator<<(std::ostream &out, lazy_string &ns);
+    /*
+        Put the string into the output stream
+        *arg out std::ostream output stream where the string is put
+        *lazy lazy lazy_string string to excract chars and put into stream
+        *return out argument
+    */
+    friend std::ostream &operator<<(std::ostream &out, lazy_string &lazy);
 
+    /*
+        returns a reference to the char at st position
+        without validation check
+        *param st - first char of the substring
+        *return if pos == size(), a reference to null char
+        *return a reference to the char
+        *throws std::out_of_range("lazy_string") if(st >= size() || st < 0)
+    */
     char_ref operator[](size_t pos);
 
     char operator[](size_t pos) const;
 
-    operator char();
-
+    /*
+        creates a std::string copy of chars of current lazy_string
+        *return a std::string copy of chars of current lazy_string
+    */
     operator std::string();
 
 private:
